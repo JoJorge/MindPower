fcab286b7fe69ae4b544c2e23632306b4f3cb02c
jiahongqu <b03901129@ntu.edu.tw>
2018-05-24 12:59:17 +0800

    add game part

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public float Destroy_Time;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("DestroyGameObject", Destroy_Time);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void DestroyGameObject()
+	{
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveHorizontal;
+	public float moveVertical;
+	public float speed = 5;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		moveHorizontal = Input.GetAxis("Horizontal");
+		moveVertical = Input.GetAxis("Vertical");
+
+		rb.AddForce(new Vector3(moveHorizontal,0f,moveVertical) * speed );
+	}
+}
