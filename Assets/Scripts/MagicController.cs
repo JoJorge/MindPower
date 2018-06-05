7308127eee17ce871164e9377975922ad90b0fb3
JoJorge <jorge13jorge13@gmail.com>
2018-06-05 19:42:53 +0800

    add Particle, MainController, MagicController, fucntion switchMagic

---
+++ Assets/Scripts/MagicController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicController : MonoBehaviour {
+
+    private bool reading;
+    // TODO: ReadingStrategy
+    private bool completeness;
+    private float power;
+    private GameObject orbCore;
+
+	void Start () {
+        reading = false;
+        completeness = false;
+        power = 0;
+	}
+	
+	void Update () {
+		
+	}
+
+    public bool isReading() {
+        return reading;
+    }
+    // TODO
+    public void setReadingStrategy() {
+    }
+    public void startReading(GameObject core) {
+        reading = true;
+        orbCore = core;
+        // TODO: start reading strategy
+    }
+    public void endReading() {
+        reading = false;
+        orbCore = null;
+        // TODO: stop reading strategy
+        if (completeness) {
+            shoot (power);
+        }
+    }
+    // TODO
+    private void shoot(float power) {
+    }
+}
---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour {
+
+    #region Variables
+    [SerializeField] private MagicMissle.MagicType nowMagicType;
+    private bool controllable;
+    [SerializeField] private MagicController magicController;
+    [SerializeField] private GameObject fireOrb;
+    [SerializeField] private GameObject iceOrb;
+    private GameObject nowOrb;
+    #endregion
+
+    #region Behaviours
+    void Start () {
+        Debug.Assert (magicController != null);
+        controllable = true;
+        if (nowMagicType == MagicMissle.MagicType.Fire) {
+            nowOrb = fireOrb;
+        }
+        else {
+            nowOrb = iceOrb;
+        }
+        nowOrb.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (controllable) {
+            if (Input.GetKeyDown (KeyCode.Space)) {
+                nowOrb.transform.Find ("Orb").gameObject.SetActive(true);
+                magicController.startReading ();
+            }
+            if (Input.GetKeyUp (KeyCode.Space)) {
+                nowOrb.transform.Find ("Orb").gameObject.SetActive(false);
+                magicController.endReading ();
+            }
+            if (Input.GetKeyDown (KeyCode.X)) {
+                switchMagicType ();
+            }
+        }
+	}
+    #endregion
+
+    #region Functions
+    public void startControl() {
+        controllable = true;
+    }
+    public void stopControl() {
+        controllable = false;
+        if (magicController.isReading ()) {
+            magicController.endReading ();
+        }
+    }
+    protected void switchMagicType() {
+        if (magicController.isReading ()) {
+            return;
+        }
+        if (nowMagicType == MagicMissle.MagicType.Fire) {
+            nowOrb.SetActive (false);
+            nowMagicType = MagicMissle.MagicType.Ice;
+            nowOrb = iceOrb;
+        }
+        else {
+            nowOrb.SetActive (false);
+            nowMagicType = MagicMissle.MagicType.Fire;
+            nowOrb = fireOrb;
+        }
+        nowOrb.SetActive (true);
+    }
+    #endregion
+}
